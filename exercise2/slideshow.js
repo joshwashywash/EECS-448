3a1a171cf24dadac4714237a979af7e0a1bfc7f5
joshwashywash <joshwashywash@gmail.com>
2017-09-22 21:23:41 -0500

    refactor code for exercise2

--- exercise2/slideshow.js
+++ exercise2/slideshow.js
@@ -11,20 +11,20 @@ let slides = {
 let index = 0;
 
 //sets the image at id=slide to the approriate image
-set = (i) => {
-  slide.src = slides[i] + ".jpg"
+setImage = (i) => {
+  if (i < 0)
+    index = 4;
+  else if (i > 4)
+    index = 0;
+  slide.src = slides[index] + ".jpg"
 }
 
 //check if out of bounds, if so then reset index
 next = () => {
-  if (++index > 4)
-    index = 0;
-  set(index);
+  setImage(++index);
 };
 
 //same logic as next()
 back = () => {
-  if (--index < 0)
-    index = 4;
-  set(index);
+  setImage(--index);
 }
