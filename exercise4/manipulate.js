8835839699e2c8ce14402e53e07698753d7190ce
joshwashywash <joshwashywash@gmail.com>
2017-09-23 20:34:42 -0500

    completed exercise4

--- exercise4/manipulate.js
+++ exercise4/manipulate.js
@@ -1,3 +1,18 @@
+alert('The HTML utilizes type="number" which may not work if you are using IE9 or earlier versions');
 color = () => {
-  document.getElementById('p').style.backgroundColor = "rgb" + document.getElementById('bgColor').value;
+  //set color of background
+  document.getElementById('p').style.backgroundColor = "rgb(" +
+    document.getElementById('bgRed').value + ", " +
+    document.getElementById('bgGreen').value + ", " +
+    document.getElementById('bgBlue').value + ")";
+};
+
+border = () => {
+  //set border width
+  document.getElementById('p').style.borderWidth = document.getElementById('brdWidth').value + "px";
+  //set border color
+  document.getElementById('p').style.borderColor = "rgb(" +
+    document.getElementById('brdRed').value + ", " +
+    document.getElementById('brdGreen').value + ", " +
+    document.getElementById('brdBlue').value + ")";
 };
