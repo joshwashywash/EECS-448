6c6a073c322d280a005d4c6b707db5f08a83f24d
Josh Oertel <oertelj@yahoo.com>
2017-09-24 13:06:45 -0500

    updated some of the formatting

--- exercise1/validator.js
+++ exercise1/validator.js
@@ -1,10 +1,8 @@
-
-
 passwordCheck = () => {
   let p1 = document.getElementById('pass').value;
   let p2 = document.getElementById('check').value;
   if (p1.length < 8)
-  alert("Yikes! The password must be more than 8 characters.");
+    alert("Yikes! The password must be more than 8 characters.");
   else if (p1 !== p2)
     alert("Oh, nuts. The passwords don't match. :(");
   else
