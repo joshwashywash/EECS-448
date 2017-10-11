1fa0e976a6505e07d47d4a1667a0a25310df82d7
joshwashywash <joshwashywash@gmail.com>
2017-10-10 22:43:35 -0500

    exercise 3 completed, i believe.

--- lab5/exercise3/createPost.php
+++ lab5/exercise3/createPost.php
@@ -1,15 +1,16 @@
 <?php
-  if (empty($_POST['username'])) {
-      exit('Username can not be empty.');
+  if (empty($_POST['post'])) {
+      exit('Post can not be empty.');
   }
   $mysql = new mysqli('mysql.eecs.ku.edu', 'joertel', 'P@$$word123', 'joertel');
   if ($mysql->connect_errno) {
       exit('Connect failed: '.$mysql->connect_error);
   }
+
   if ($mysql->query("SELECT * FROM Users WHERE ID = '{$_POST['username']}'")->num_rows) {
-      echo 'Username already exists in the database.';
+      $mysql->query("INSERT INTO Posts (content, author) VALUES ('{$_POST['post']}', '{$_POST['username']}')");
+      echo 'Your post was saved in the database.';
   } else {
-      $mysql->query("INSERT INTO Users (ID) VALUES ('{$_POST['username']}')");
-      echo 'Username has been added to the database.';
+      echo 'Username was not found in the database. Post rejected.';
   }
   $mysql->close();
